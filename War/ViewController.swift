6e22788b012f5d25394cf3dc5ce612e10818a9b2
Mostafa <MostafaAbdEl_Fatah@192.168.179.128>
2017-10-07 09:58:03 -0700

    arc4random_uniform

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var num = 0
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    var words = ["hello In IOS","I'm Very Busy","Welcome in IOS","I'm Not Happy","I'm Happy"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        btn1.setTitle("PLAY Loaded", forState: UIControlState.Normal)
+        
+        let num1 = Int.init(arc4random_uniform(5))
+        label.text = String(format: "Button Clicked %i times" , num )
+        label2.text = words[num1]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btn1_Clicked(sender: UIButton) {
+        let num1 = Int(arc4random_uniform(5))
+        btn1.setTitle("PlAY", forState: UIControlState.Normal)
+        label.text = String(format: "Button Clicked %i times" , ++num )
+        label2.text = words[num1]
+    }
 
 }
 
